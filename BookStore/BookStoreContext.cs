f64fac3bdfaadd992090cdb79531f02e456ecfe5
Rodrigo <rodrigov05ricardo@gmail.com>
2025-01-20 18:41:51 +0000

    Commit Db

---
+++ BookStore/BookStoreContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore
+{
+    public class BookStoreContext : IdentityDbContext
+    {
+        public BookStoreContext(DbContextOptions<BookStoreContext> options)
+            : base(options)
+        {
+        }
+    }
+}
--- BookStore/Program.cs
+++ BookStore/Program.cs
@@ -1,4 +1,4 @@
-using BookStore.Data;
+using BookStore;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -6,12 +6,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
+builder.Services.AddDbContext<BookStoreContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+    .AddEntityFrameworkStores<BookStoreContext>();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
