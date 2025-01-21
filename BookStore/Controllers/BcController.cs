8b8795eb179d6e2fde83d0eef31a0574cb047090
Rodrigo <rodrigov05ricardo@gmail.com>
2025-01-21 16:43:54 +0000

    details and back home

--- BookStore/Controllers/BcController.cs
+++ BookStore/Controllers/BcController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SQLitePCL;
 
 namespace BookStore.Controllers
 {
@@ -14,6 +16,21 @@ namespace BookStore.Controllers
         {
             var Books = context.Books.OrderByDescending(p => p.Id).ToList();
             return View(Books);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult Details(int id)
+        {
+            var book = context.Books.FirstOrDefault(b => b.Id == id);
+
+            if (book == null)
+            {
+                return NotFound(); // Or return a different error view
+            }
+
+            return View(book); // Pass the book object as the model
         }
     }
 }
