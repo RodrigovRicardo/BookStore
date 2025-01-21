4b3bfd40061577ae6b46f10e1b9c6c967fe0b560
Rodrigo <rodrigov05ricardo@gmail.com>
2025-01-21 22:09:12 +0000

    Edit and remove fuctions

--- BookStore/Controllers/BcController.cs
+++ BookStore/Controllers/BcController.cs
@@ -1,33 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookStore.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.Blazor;
 using SQLitePCL;
 
 namespace BookStore.Controllers
 {
     public class BcController : Controller
     {
-        private readonly BookStoreContext context;
+        private readonly ILogger<HomeController> _logger;
+        private readonly BookStoreContext _context;
 
-        public BcController(BookStoreContext context)
+        public BcController(ILogger<HomeController> logger, BookStoreContext context)
         {
-            this.context = context;
+            _logger = logger;
+            _context = context;
         }
         public IActionResult Index()
         {
-            var Books = context.Books.OrderByDescending(p => p.Id).ToList();
+            var Books = _context.Books.OrderByDescending(p => p.Id).ToList();
             return View(Books);
         }
         public IActionResult Create()
         {
             return View();
         }
-        public IActionResult Edit()
-        {
-            return View();
-        }
+
         public IActionResult Details(int id)
         {
-            var book = context.Books.FirstOrDefault(b => b.Id == id);
+            var book = _context.Books.FirstOrDefault(b => b.Id == id);
 
             if (book == null)
             {
@@ -36,5 +38,56 @@ namespace BookStore.Controllers
 
             return View(book); // Pass the book object as the model
         }
+
+        public IActionResult Edit(int id)
+        {
+            var book = _context.Books.FirstOrDefault(p => p.Id == id);
+
+            return View(book);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Book book)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.Books.Update(book);
+                _context.SaveChanges();
+
+                return RedirectToAction("Backoffice", "Home");
+            }
+            // Log the model state errors to the console
+            foreach (var state in ModelState)
+            {
+                foreach (var error in state.Value.Errors)
+                {
+                    _logger.LogError($"Property: {state.Key}, Error: {error.ErrorMessage}");
+                }
+            }
+
+            return View(book);
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var book = _context.Books.FirstOrDefault(p => p.Id == id);
+                
+            if(book != null)
+            {
+                _context.Books.Remove(book);
+                _context.SaveChanges();
+            }
+            // Log the model state errors to the console
+            foreach (var state in ModelState)
+            {
+                foreach (var error in state.Value.Errors)
+                {
+                    _logger.LogError($"Property: {state.Key}, Error: {error.ErrorMessage}");
+                }
+            }
+            return RedirectToAction("Backoffice","Home");
+        }
+
     }
 }
